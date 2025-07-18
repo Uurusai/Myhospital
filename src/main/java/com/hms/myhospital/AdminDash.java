f979352889030479aff0269d66b7a440a3208b92
Uurusai <adibadibamorshed@gmail.com>
2025-07-18 20:56:10 +0600

    updated

--- src/main/java/com/hms/myhospital/AdminDash.java
+++ src/main/java/com/hms/myhospital/AdminDash.java
@@ -1,4 +1,62 @@
-package com.hms.myhospital;
+    package com.hms.myhospital;
 
-public class AdminDash {
-}
+    import javafx.event.ActionEvent;
+    import javafx.fxml.FXML;
+    import javafx.fxml.Initializable;
+    import javafx.scene.control.Button;
+    import javafx.scene.layout.AnchorPane;
+
+    import java.net.URL;
+    import java.util.ResourceBundle;
+
+
+    public class AdminDash implements Initializable {
+
+        @FXML private Button doctor_btn ;
+        @FXML private Button db_btn ;
+        @FXML private Button patient_btn ;
+        @FXML private Button appointment_btn ;
+        @FXML private AnchorPane patientsView;
+        @FXML private AnchorPane doctorsView;
+        @FXML private AnchorPane appointmentsView;
+        @FXML private AnchorPane db_top;
+        @FXML private AnchorPane db_btm;
+
+
+
+        public void switchForm(ActionEvent event){
+            if(event.getSource() == doctor_btn){
+                setAllInvisible();
+                doctorsView.setVisible(true);
+            }
+            else if(event.getSource() == patient_btn){
+                setAllInvisible();
+                patientsView.setVisible(true);
+            }
+            else if(event.getSource() == appointment_btn){
+                setAllInvisible();
+                appointmentsView.setVisible(true);
+            }
+            else if(event.getSource() == db_btn){
+                setAllInvisible();
+                db_btm.setVisible(true);
+                db_top.setVisible(true);
+            }
+        }
+
+        private void setAllInvisible() {
+            db_btm.setVisible(false);
+            db_top.setVisible(false);
+            doctorsView.setVisible(false);
+            patientsView.setVisible(false);
+            appointmentsView.setVisible(false);
+        }
+
+        @Override
+        public void initialize(URL url, ResourceBundle resourceBundle) {
+            setAllInvisible();
+            db_btm.setVisible(false);
+            db_top.setVisible(false);
+
+        }
+    }
--- src/main/java/com/hms/myhospital/HelloApplication.java
+++ src/main/java/com/hms/myhospital/HelloApplication.java
@@ -2,6 +2,7 @@ package com.hms.myhospital;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,14 +11,14 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("admin-dash.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/hms/myhospital/admin-dash.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("MyHospital");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
